4cd36dc0fc9805b97526c340aed192cd01bb757a
hb2000 <sainounomudazukai1235@gmail.com>
2024-11-21 22:29:58 +0900

    vol5

--- timer.js
+++ timer.js
@@ -1,19 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
     // 指定日付を設定
-    const targetDate = '2024/09/21';
+    const targetDate = '2024/11/22';
     // 指定日時のタイムスタンプを作成
-    const targetTime = new Date(`${targetDate} 12:00:00`).getTime();
+    const targetTime = new Date(`${targetDate} 18:00:00`).getTime();
 
     // 現在のタイムスタンプを取得
     const currentTime = new Date().getTime();
 
-    // 今日の12時を過ぎているか確認
+    // 過ぎているか確認
     if (currentTime >= targetTime) {
-        // 12時を過ぎていたら表示
+        // 過ぎていたら表示
         document.querySelector('.flyer').style.display = 'block';
         document.querySelector('iframe').style.width = '100%';
     } else {
-        // 12時をまだ過ぎていなかったら12時になった時点で表示
+        // まだ過ぎていなかったらなった時点で表示
         setTimeout(function() {
             document.querySelector('.flyer').style.display = 'block';
             document.querySelector('iframe').style.width = '100%';
